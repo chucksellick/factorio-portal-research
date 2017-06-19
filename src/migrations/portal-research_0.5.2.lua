0464a6935642fe6cd08d0c4e5b5be9336d4a2aa5
petehurst <petehurst@users.noreply.github.com>
2017-06-19 19:28:13 +0100

    Clean up merge

--- src/migrations/portal-research_0.5.2.lua
+++ src/migrations/portal-research_0.5.2.lua
@@ -1,5 +1,16 @@
 for i, force in pairs(game.forces) do
   force.reset_technologies()
-  force.reset_technology_effects()
   force.reset_recipes()
+  if force.technologies["rocket-silo"] and force.technologies["rocket-silo"].researched then
+    -- Some additional components need to be available to build satellites
+    local unlock_silo_recipes = {
+      -- "communications-system" or "communication-systems" ??
+      -- "satellite-housing"
+      "navigation-computer",
+      "solar-array"
+    }
+    for i,t in pairs(unlock_silo_recipes) do
+      force.recipes[t].enabled = true
+    end
+  end
 end
